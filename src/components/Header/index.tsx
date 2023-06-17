7488a9f9f36a3a7accf45774197a585a385cc365
Gabriel Jesus <gabriel.developer2@gmail.com>
2023-06-17 10:26:17 -0300

    add lib heroicons and initial responsive layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import Logo from '../../assets/logo.png'
+import Logo from '../../../assets/logo.png'
+import { Bars3Icon } from '@heroicons/react/24/outline'
 
 export function Header() {
   return (
-    <header className="w-full h-22 flex items-center justify-end shadow shadow-lg bg-white">
+    <header className="w-full h-22 flex items-center justify-end shadow shadow-lg bg-white ">
       <div className="w-full flex items-center justify-between ml-10">
         <Image
           src={Logo}
@@ -12,10 +13,13 @@ export function Header() {
           height={200}
           alt="Logo GJ consultoria em T.I"
         />
-        <nav className="gap-2  m-10 space-x-40 flex-row w-[50%] items-center justify-center flex">
+        <nav className="gap-2  m-10 space-x-40 flex-row w-[50%] items-center justify-center flex ">
           <NavLinks title="Empresa" />
           <NavLinks title="Contato" />
           <NavLinks title="Serviços" />
+          <button>
+            <Bars3Icon className="w-6 h-6" />
+          </button>
         </nav>
       </div>
     </header>
@@ -29,7 +33,7 @@ function NavLinks({ title }: LinksProps) {
   return (
     <Link
       href=""
-      className="font-text hover:font-bold text-md text-black-900  hover:border-b-2 hover:text-blue-900 "
+      className="font-text hover:font-bold text-md text-blue-600 hover:border-b-blue-900 hover:border-b-2 hover:text-blue-900 "
     >
       <p className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 ">
         {title}
